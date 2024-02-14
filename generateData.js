ba64abaa696a67af9363aaadd7a4c663c0feca53
Walter Benavides <wbenafi@gmail.com>
2024-02-14 08:34:01 -0600

    Fix data to be accurate with the timezone

--- generateData.js
+++ generateData.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 
 async function getMoonData({ month, year }) {
     const params = new URLSearchParams({ month: String(month), year: String(year) });
+    params.set('LDZ', String(new Date(year, month-1 , 1).getTime() / 1000));
     const response = await fetch(`https://www.icalendar37.net/lunar/api/?${params.toString()}`);
     const json = response.json();
     return json;
@@ -16,6 +17,10 @@ async function generateMoonData() {
         const yearData = [];
         for (let month = 1; month <= 12; month++) {
             const data = await getMoonData({ month, year });
+            Object.values(data.phase).forEach((phase) => {
+                delete phase.svg;
+                delete phase.svgMini;
+            });
             yearData.push(data);
         }
         moonData[year] = yearData;
--- src/services/moonApi.tsx
+++ src/services/moonApi.tsx
@@ -1,8 +1,9 @@
 
 
 // export async function getMoonData({ month, year } : { month: number, year: number }) {
-
 //     const params = new URLSearchParams({ month: String(month), year: String(year) });
+
+//     params.set('LDZ', String(new Date(year, month-1 , 1).getTime() / 1000));
 
 //     const response = await fetch(`https://www.icalendar37.net/lunar/api/?${params.toString()}`);
 
